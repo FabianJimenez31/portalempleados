d03ccf915a850bebc0e268c065f340f97ad5d043
Fabian Jimenez <fabian.jimenez.ing@gmail.com>
2016-10-24 13:07:57 -0500

    Parte de empleador, crear y eliminar empleados

---
+++ js/main.js
@@ -0,0 +1,59 @@
+(function(){
+	// Variables
+	var lista = document.getElementById("lista"),
+		empleoInput = document.getElementById("empleoInput"),
+		btnNuevaTarea = document.getElementById("btn-agregar");
+
+	// Funciones
+	var agregarTarea = function(){
+		var tarea = empleoInput.value,
+			nuevaTarea = document.createElement("li"),
+			enlace = document.createElement("a"),
+			contenido = document.createTextNode(tarea);
+
+		if (tarea === "") {
+			empleoInput.setAttribute("placeholder", "Agrega una tarea valida");
+			empleoInput.className = "error";
+			return false;
+		}
+
+		// Agregamos el contenido al enlace
+		enlace.appendChild(contenido);
+		// Le establecemos un atributo href
+		enlace.setAttribute("href", "#");
+		// Agrergamos el enlace (a) a la nueva tarea (li)
+		nuevaTarea.appendChild(enlace);
+		// Agregamos la nueva tarea a la lista
+		lista.appendChild(nuevaTarea);
+
+		empleoInput.value = "";
+
+		for (var i = 0; i <= lista.children.length -1; i++) {
+			lista.children[i].addEventListener("click", function(){
+				this.parentNode.removeChild(this);
+			});
+		}
+
+	};
+	var comprobarInput = function(){
+		empleoInput.className = "";
+		teareaInput.setAttribute("placeholder", "Agrega tu tarea");
+	};
+
+	var eleminarTarea = function(){
+		this.parentNode.removeChild(this);
+	};
+
+	// Eventos
+
+	// Agregar Tarea
+	btnNuevaTarea.addEventListener("click", agregarTarea);
+
+	// Comprobar Input
+	empleoInput.addEventListener("click", comprobarInput);
+
+	// Borrando Elementos de la lista
+	for (var i = 0; i <= lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click", eleminarTarea);
+	}
+}());
